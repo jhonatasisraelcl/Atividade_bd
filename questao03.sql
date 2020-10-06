b18167229c0833b2c8d38bd4ffd4aca8c49822b9
Jhonatas Laurentino <costajhonatas9@gmail.com>
2020-10-05 21:56:56 -0300

    Update questao03.sql

--- questao03.sql
+++ questao03.sql
@@ -7,48 +7,56 @@ NEXT VALUE FOR
 	AUTO INCREMENT
 Considere o banco de dados abaixo para responder às questões 1 a 6:
 *obs: primary key, foreign key.
+
 cliente (codigo, nome, endereco)
 piloto (codigo, nome, num_voos)
 voo (codigo, tipo, piloto, num_passageiros, distancia)
 milhas (cliente, quantidade)
 cliente_voo (cliente, voo, classe)
 */
-CREATE TABLE cliente(
-	codigo CHAR(9),
-	nome VARCHAR(20) NOT NULL,
-	endereco VARCHAR(255),
-	PRIMARY KEY (codigo)
-);
 
-CREATE TABLE piloto(
-	codigo CHAR(9),
-	nome VARCHAR(20) NOT NULL,
-	num_voos INT,
-	PRIMARY KEY (codigo)
+CREATE SEQUENCE sequencia  START WITH 1 increment BY 1;
+CREATE SEQUENCE sequencia2 START WITH 1 increment BY 1;
+CREATE SEQUENCE sequencia3 START WITH 1 increment BY 1;
+CREATE SEQUENCE sequencia4 START WITH 1 increment BY 1;
+ 
+CREATE TABLE cleinte (
+    codigo INT default (NEXT VALUE for sequencia),
+    nome VARCHAR(150),
+    endereco VARCHAR(150),
+	  CONSTRAINT pk_cliente PRIMARY KEY(codigo)
 );
-
-CREATE TABLE voo(
-	codigo CHAR(9),
-	tipo VARCHAR(20) NOT NULL,
-	piloto CHAR(9),
-	num_passageiros INT, 
-	distancia DECIMAL(10,2),
-	PRIMARY KEY (codigo),
-	FOREIGN KEY (piloto) REFERENCES piloto(codigo)
+ 
+CREATE TABLE piloto (
+    codigo INT default (NEXT VALUE for sequencia2),
+    nome VARCHAR(150),
+    num_voos VARCHAR(150),
+	  CONSTRAINT pk_piloto PRIMARY KEY(codigo)
 );
-
-CREATE TABLE milhas(
-	cliente CHAR(9), 
-	quantidade INT,
-	PRIMARY KEY (cliente),
-	FOREIGN KEY (cliente) REFERENCES cliente(codigo)
+ 
+CREATE TABLE voo (
+    codigo INT default (NEXT VALUE for sequencia3),
+    piloto INT,
+    tipo VARCHAR(150),
+	  num_passageiros VARCHAR(150),
+	  distancia INT,
+	  FOREIGN KEY (piloto) REFERENCES piloto(codigo),
+	  CONSTRAINT pk_voo PRIMARY KEY(codigo)
 );
-
-CREATE TABLE cliente_voo(
-	cliente CHAR(9), 
-	voo CHAR(9), 
-	classe VARCHAR(20),
-	PRIMARY KEY (cliente, voo),
-	FOREIGN KEY (cliente) REFERENCES cliente(codigo),
-	FOREIGN KEY (voo) REFERENCES voo(codigo)
+ 
+CREATE TABLE cliente_voo (
+    cliente INT,
+	  voo INT,
+    classe VARCHAR(150),
+	  FOREIGN KEY (cliente) REFERENCES cliente(codigo),
+	  FOREIGN KEY (voo) REFERENCES voo(codigo),
+	  CONSTRAINT pk_cliente_voo PRIMARY KEY(cliente, voo)
+);
+ 
+create table milhas (
+    codigo INT default (NEXT VALUE for sequencia4),
+    cliente INT,
+    qtde INT,
+	  FOREIGN KEY (cliente) REFERENCES cliente(codigo),
+	  CONSTRAINT pk_milhas PRIMARY KEY(codigo)
 );
