8a48d33f55d7359c0c22579ed408fc57c9c1ff1c
Jhonatas Laurentino <costajhonatas9@gmail.com>
2020-10-05 23:34:45 -0300

    Update questao012.sql

--- questao012.sql
+++ questao012.sql
@@ -1,6 +1,2 @@
--- OBS: Use um banco de dados e execute para testar o sql. 
-
 create sequence contador start with 1 increment by 1 maxvalue=1000;
 select nextval(contador);
-
--- O valor de "contador" começa em (1) guido de (2), (3)... até (1000).
