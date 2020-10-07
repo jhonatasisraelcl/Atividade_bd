787edfc1ff3a800a24772cfd001744ccae2181dc
Jhonatas Laurentino <costajhonatas9@gmail.com>
2020-10-06 21:52:11 -0300

    Update questao01.sql

--- ATV3/questao01.sql
+++ ATV3/questao01.sql
@@ -1,9 +1,9 @@
 /*Faça uma consulta que selecione o nome dos funcionários que recebem salários superiores aos salários pagos no departamento 5.
 */
-SELECT Nome
-FROM Funcionario
-WHERE Salario > (
-	SELECT Salario
-	FROM Funcionario fdep5
+SELECT nome
+FROM funcionario
+WHERE salario > (
+	SELECT salario
+	FROM funcionario fdep5
 	WHERE fdep5.coddepto = '5'
 );
