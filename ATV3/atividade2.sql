5fe33585b1bf4485f4b5e9296770cc091a895d5e
Jhonatas Laurentino <costajhonatas9@gmail.com>
2020-10-07 21:54:07 -0300

    Create atividade2.sql

---
+++ ATV3/atividade2.sql
@@ -0,0 +1,81 @@
+create database atividade2;
+use atividade2;
+
+create sequence sequencia1 start with 1 increment by 1;
+create sequence sequencia2 start with 1 increment by 1;
+create sequence sequencia3 start with 1 increment by 1;
+create sequence sequencia4 start with 1 increment by 1;
+create sequence sequencia5 start with 1 increment by 1;
+create sequence sequencia6 start with 1 increment by 1;
+
+create table atividade(
+    codigo int default (next value for sequencia1),
+	descricao varchar(150),
+    
+	constraint pk_atividade primary key(codigo)
+);
+
+create table funcionario(
+    codigo int default (next value for sequencia2),
+	nome varchar(150),
+	sexo char,
+	data_nasc date,
+	salario decimal,
+	cod_depto int,
+    
+	-- foreign key cod_depto references departamento(codigo),
+	constraint pk_funcionario primary key(codigo)
+);
+
+create table gerente(
+    codigo int default (next value for sequencia6),
+    cod_func int,
+	cod_depto int,
+    
+	-- foreign key cod_func references funcionario(codigo),
+	-- foreign key cod_depto references departamento(codigo),
+    constraint pk_gerente primary key(codigo)
+);
+
+create table departamento(
+    codigo int default (next value for sequencia3),
+	descricao varchar(150),
+	cod_gerente int,
+    
+	-- foreign key cod_gerente references gerente(codigo),
+	constraint pk_departamento primary key(codigo)
+);
+
+create table projeto(
+    codigo int default (next value for sequencia4),
+	descricao varchar(150),
+	cod_depto int,
+	cod_responsavel int,
+	data_inicio date,
+	data_fim date,
+    
+	-- foreign key cod_responsavel references funcionario(codigo),
+	-- foreign key cod_gerente references gerente(codigo),
+	constraint pk_projeto primary key(codigo)
+);
+
+
+create table atividade_projeto(
+    codigo int default (next value for sequencia5),
+	cod_projeto int,
+	cod_atividade int,
+	cod_responsavel int,
+	data_inicio date,
+	data_fim date,
+    
+	-- foreign key cod_responsavel references funcionario(codigo),
+	-- foreign key cod_gerente references gerente(codigo),
+	-- foreign key cod_projeto references projeto(codigo),
+	constraint pk_atividade_projeto primary key(codigo)
+);
+
+ALTER TABLE nome_da_tabela ADD CONSTRAINT nome_da_constraint
+FOREIGN KEY(nome_da_coluna_local) REFERENCES nome_da_tabela_da_fk(coluna_fk);
+
+alter table funcionario add constraint fk_cod_depto
+foreign key (cod_depto) references departamento(codigo);
