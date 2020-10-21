35bdb7cf7222da27e7ba9ab08996cbb7e13c4575
Jhonatas Laurentino <costajhonatas9@gmail.com>
2020-10-20 23:02:47 -0300

    Update questao04.sql

--- ATV4/questao04.sql
+++ ATV4/questao04.sql
@@ -1,2 +1,7 @@
 /*Crie um usuário chamado empresa_admim para o banco de dados EmpresaDB. 
 Este usuário deve ter todos os privilégios mas com acesso remoto.*/
+
+CREATE DATABASE EmpresaDB;
+CREATE user 'empresa_admim'@'%' identified BY 'admin';
+USE empresadb;
+GRANT all privileges ON empresadb TO 'empresa_admim'@'%';
