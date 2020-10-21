200931fad144438104b41ed0ade38a35df43557e
Jhonatas Laurentino <costajhonatas9@gmail.com>
2020-10-20 22:55:03 -0300

    Update questao03.sql

--- ATV4/questao03.sql
+++ ATV4/questao03.sql
@@ -1,7 +1,7 @@
 /*Crie um usuário chamado empresa_admim_local para o banco de dados EmpresaDB. 
 Este usuário deve ter todos os privilégios mas sem acesso remoto.*/
 
-create database EmpresaDB;
-create user 'empresa_admim_local'@'localhost' identified by 'admin';
-use empresadb;
-grant all privileges on empresadb to 'empresa_admim_local'@'localhost';
+CREATE DATABASE EmpresaDB;
+CREATE user 'empresa_admim_local'@'localhost' identified BY 'admin';
+USE empresadb;
+GRANT all privileges ON empresadb TO 'empresa_admim_local'@'localhost';
