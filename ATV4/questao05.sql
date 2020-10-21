504b769cc4906338ec64cb269ef2d063c0efd6d2
Jhonatas Laurentino <costajhonatas9@gmail.com>
2020-10-20 23:06:41 -0300

    Update questao05.sql

--- ATV4/questao05.sql
+++ ATV4/questao05.sql
@@ -1,3 +1,8 @@
 /*Crie um usuário chamado empresa_gerente para o banco de dados EmpresaDB. 
 Este usuário deve ter privilégios apenas de fazer consultas em todas as tabelas. 
 Ele pode inserir e atualizar as tabelas Equipe, Membro, Atividade e AtividadeProjeto.*/
+  
+CREATE DATABASE EmpresaDB;
+CREATE user 'empresa_gerente'@'%' identified BY 'admin';
+USE empresadb;
+GRANT SELECT, INSERT, UPDATE ON empresadb TO 'empresa_gerente'@'%';
