5f3321fa4b76f1b65a4d8b4beffe3d8ba69141c7
Jhonatas Laurentino <costajhonatas9@gmail.com>
2020-10-20 23:09:06 -0300

    Update questao07.sql

--- ATV4/questao07.sql
+++ ATV4/questao07.sql
@@ -1,2 +1,7 @@
 /*Crie um índice para o atributo sigla da tabela departamento e um índice para o 
 atributo situação da tabela atividade. */
+CREATE index index_departameto
+ON departamento(sigla);
+
+CREATE index index_atividade
+ON atividade(situacao);
