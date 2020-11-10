2bc19302a543a507715073c74286644bf91d223c
Jhonatas Laurentino <costajhonatas9@gmail.com>
2020-11-10 12:30:46 -0300

    Update questao02.sql

--- ATV5/questao02.sql
+++ ATV5/questao02.sql
@@ -1,1 +1,16 @@
 /*Faça uma consulta para calcular a média de idade por departamento*/
+DROP FUNCTION IF EXISTS media_idade_por_dep cascade;
+CREATE OR REPLACE FUNCTION media_idade_por_dep(INTEGER) 
+  RETURNS TABLE (idade INTERVAL) 
+  LANGUAGE 'plpgsql' 
+  AS $$
+BEGIN
+	RETURN QUERY(
+		SELECT AVG(AGE(f.datanasc)) AS idade
+		FROM funcionario f
+		WHERE f.depto = $1
+	);
+END;
+$$;
+
+SELECT media_idade_por_dep(1);
