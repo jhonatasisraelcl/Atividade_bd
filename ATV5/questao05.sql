0684c214a90983c5cc6538968e57e75f961de46a
Jhonatas Laurentino <costajhonatas9@gmail.com>
2020-11-11 14:46:13 -0300

    Update questao05.sql

--- ATV5/questao05.sql
+++ ATV5/questao05.sql
@@ -1,3 +1,18 @@
 /*Faça uma função para calcular quantos dias de atraso tem um projeto. 
 A função retornará negativo se tiver atrasada e positivo se tiver em dia.
 */
+CREATE OR REPLACE FUNCTION questao_5(codProjeto INTEGER) 
+RETURNS INTERVAL AS $$
+DECLARE
+	fim DATE;
+	dataHoje TIMESTAMP;
+	diasAtraso INTERVAL;
+BEGIN
+	SELECT INTO datahoje NOW(); 
+	SELECT INTO fim datafim FROM projeto WHERE codigo = codProjeto;
+	diasAtraso = fim - dataHoje;
+	RETURN diasAtraso;
+END;
+$$ LANGUAGE 'plpgsql';
+
+SELECT questao_5('1');
