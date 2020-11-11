2c6f7f49323848d6db74a87aa845cbce75325a77
Jhonatas Laurentino <costajhonatas9@gmail.com>
2020-11-11 10:17:58 -0300

    Update questao04.sql

--- ATV5/questao04.sql
+++ ATV5/questao04.sql
@@ -1,2 +1,21 @@
 /*Faça uma função para calcular quantos dias de atraso tem uma atividade. 
 A função retornará negativo se tiver atrasada e positivo se tiver em dia.*/
+CREATE OR REPLACE FUNCTION atrasou(codAtividade INTEGER) 
+RETURNS INTERVAL 
+LANGUAGE 'plpgsql'
+AS $$
+DECLARE
+	fim DATE;
+	dataHoje TIMESTAMP;
+	diasAtraso INTERVAL;
+BEGIN
+	SELECT INTO datahoje NOW(); 
+	SELECT INTO fim datafim  
+    FROM atividade 
+    WHERE codigo = codAtividade;
+	diasAtraso = fim - dataHoje;
+	RETURN diasAtraso;
+END;
+$$;
+
+SELECT atrasou('1');
