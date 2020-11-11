e2f5568f51b32ddb14df6289b705df055ed1a5b9
Jhonatas Laurentino <costajhonatas9@gmail.com>
2020-11-11 10:14:16 -0300

    Update questao03.sql

--- ATV5/questao03.sql
+++ ATV5/questao03.sql
@@ -1,1 +1,11 @@
 /*Faça um procedimento para exibir os funcionários com idade acima da média do seu departamento.*/
+CREATE OR REPLACE PROCEDURE acima_da_media()
+LANGUAGE 'plpgsql'
+AS $$
+BEGIN
+	SELECT nome 
+  FROM funcionario 
+  WHERE calcula_idade_func(codigo) > media_idade_por_dep(depto);
+END;
+$$;
+call acima_da_media();
