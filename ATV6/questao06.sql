417bb95250afa72db64f615d5ee34464df8be5bd
Jhonatas Laurentino <costajhonatas9@gmail.com>
2020-11-12 00:00:18 -0300

    Update questao06.sql

--- ATV6/questao06.sql
+++ ATV6/questao06.sql
@@ -1,1 +1,10 @@
 /*Crie uma procedure que recebe um valor qualquer e retorna esse valor ao quadrado.*/
+CREATE OR REPLACE PROCEDURE quadrado(INOUT num integer)
+LANGUAGE 'plpgsql'
+AS $$
+BEGIN
+	num = num^2;
+END;
+$$;
+
+CALL quadrado(4);
