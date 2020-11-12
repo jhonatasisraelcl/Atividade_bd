1bd631366c697b7a133f477411e1b27e7b63eaa7
Jhonatas Laurentino <costajhonatas9@gmail.com>
2020-11-11 23:19:52 -0300

    Update questao02.sql

--- ATV6/questao02.sql
+++ ATV6/questao02.sql
@@ -1,2 +1,18 @@
 /*Crie uma função que recebe o código de um cliente e o código de um voo e retorna a 
 classe que o cliente está viajando.*/
+CREATE OR REPLACE FUNCTION qualclasse(codCliente CHAR(9), codVoo CHAR(9)) 
+RETURNS VARCHAR(20)
+LANGUAGE 'plpgsql'
+AS $$
+DECLARE
+	classeVoo VARCHAR(20);
+BEGIN
+	SELECT INTO classeVoo classe 
+  FROM cliente_voo 
+  WHERE cliente = codCliente 
+  AND voo = codVoo;
+	RETURN classeVoo;
+END;
+$$;
+
+SELECT qualclasse('1', '1');
