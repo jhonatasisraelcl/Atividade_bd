62a6975f37edde80c89e3d4d90231796b5a26418
Jhonatas Laurentino <costajhonatas9@gmail.com>
2020-11-11 23:15:35 -0300

    Update questao01.sql

--- ATV6/questao01.sql
+++ ATV6/questao01.sql
@@ -1,1 +1,14 @@
 /*Crie uma função que recebe o código de um voo e retorna o número de passageiros desse voo.*/
+CREATE OR REPLACE FUNCTION boravoar(codVoo CHAR(9)) 
+  RETURNS INTEGER 
+  LANGUAGE 'plpgsql'
+  AS $$
+DECLARE
+	numPassageiros INTEGER;
+BEGIN
+	SELECT INTO numPassageiros num_passageiros FROM voo WHERE codigo = codVoo;
+	RETURN numPassageiros;
+END;
+$$;
+
+SELECT boravoar('1');
