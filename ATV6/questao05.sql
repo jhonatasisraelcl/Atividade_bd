858beea2036dad3bc03d9d8f8c42c9d5e40a69ba
Jhonatas Laurentino <costajhonatas9@gmail.com>
2020-11-11 23:41:30 -0300

    Update questao05.sql

--- ATV6/questao05.sql
+++ ATV6/questao05.sql
@@ -1,1 +1,16 @@
 /*Crie uma procedure que recebe um valor qualquer e retorna esse valor ao quadrado*/
+CREATE OR REPLACE FUNCTION questao_5()
+RETURNS INTEGER 
+LANGUAGE 'plpgsql'
+AS $$
+DECLARE
+	quantClientes INTEGER;
+BEGIN
+	SELECT INTO quantClientes COUNT(*) 
+  FROM milhas
+   WHERE quantidade > 1200;
+	RETURN quantClientes;
+END;
+$$;
+
+SELECT questao_5();
