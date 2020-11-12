38f569b4e90bed761824dfb2f3d41ea088ae6146
Jhonatas Laurentino <costajhonatas9@gmail.com>
2020-11-11 22:55:14 -0300

    Update questao13.sql

--- ATV5/questao13.sql
+++ ATV5/questao13.sql
@@ -1,2 +1,11 @@
 /*Crie um índice para o atributo sigla da tabela departamento e um índice para o atributo situação da 
 tabela atividade.*/
+DROP  INDEX IF EXISTS index_departamento_sigla;
+CREATE  INDEX  index_departamento_sigla  
+ON departamento 
+USING btree (sigla);
+
+DROP  INDEX IF EXISTS index_atividade_situacao;
+CREATE  INDEX  index_atividade_situacao  
+ON atividade 
+USING btree (situacao);
