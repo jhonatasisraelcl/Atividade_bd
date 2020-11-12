0178680578fb2c3aa96171be994e20ab5a8d525c
Jhonatas Laurentino <costajhonatas9@gmail.com>
2020-11-11 23:57:27 -0300

    Update questao05.sql

--- ATV6/questao05.sql
+++ ATV6/questao05.sql
@@ -1,5 +1,6 @@
-/*Crie uma procedure que recebe um valor qualquer e retorna esse valor ao quadrado*/
-CREATE OR REPLACE FUNCTION questao_5()
+/*Crie uma função que retorne o número de clientes com mais de 500 milhas.*/
+
+CREATE OR REPLACE FUNCTION maisDe500()
 RETURNS INTEGER 
 LANGUAGE 'plpgsql'
 AS $$
@@ -7,10 +8,10 @@ DECLARE
 	quantClientes INTEGER;
 BEGIN
 	SELECT INTO quantClientes COUNT(*) 
-  FROM milhas
-   WHERE quantidade > 1200;
+  FROM milhas 
+  WHERE quantidade > 500;
 	RETURN quantClientes;
 END;
 $$;
 
-SELECT questao_5();
+SELECT maisDe500();
