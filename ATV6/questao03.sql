822b3a87942562fd531c2715338ba858c4aaaa0d
Jhonatas Laurentino <costajhonatas9@gmail.com>
2020-11-11 23:30:00 -0300

    Update questao03.sql

--- ATV6/questao03.sql
+++ ATV6/questao03.sql
@@ -1,1 +1,16 @@
 /*Crie uma função que  retorna o número de voos que certo cliente já realizou.*/
+CREATE OR REPLACE FUNCTION questao_3(codCliente CHAR(9)) 
+RETURNS INTEGER 
+LANGUAGE 'plpgsql'
+AS $$
+DECLARE
+	voosCliente INTEGER;
+BEGIN
+	SELECT INTO voosCliente count(*) 
+  FROM cliente_voo 
+  WHERE cliente = codCliente;
+	RETURN voosCliente;
+END;
+$$;
+
+SELECT questao_3('1');
