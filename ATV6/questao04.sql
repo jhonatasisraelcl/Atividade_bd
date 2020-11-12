1b7403e66e264dd615f981fc4e6b8e90b9b68ceb
Jhonatas Laurentino <costajhonatas9@gmail.com>
2020-11-11 23:35:12 -0300

    Update questao04.sql

--- ATV6/questao04.sql
+++ ATV6/questao04.sql
@@ -1,2 +1,15 @@
-/*Crie uma função que retorna a soma das milhas de todos os clientes.
-*/
+/*Crie uma função que retorna a soma das milhas de todos os clientes.*/
+CREATE OR REPLACE FUNCTION somaMilhas()
+RETURNS INTEGER 
+LANGUAGE 'plpgsql'
+AS $$
+DECLARE
+	totalMilhas INTEGER;
+BEGIN
+	SELECT INTO totalMilhas sum(quantidade) 
+  FROM milhas;
+	RETURN totalMilhas;
+END;
+$$;
+
+SELECT somaMilhas();
